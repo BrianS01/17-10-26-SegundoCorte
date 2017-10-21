425f586b1f884edcf442cb0e3df6f13db69cf4a7
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-10-20 20:17:59 -0500

    01. Creacion

--- src/main/java/connection/DB.java
+++ src/main/java/connection/DB.java
@@ -1,6 +1,9 @@
 /*
  *  PROYECTO SEGUNDO CORTE
- *      Author ::: Brian Sterling
+ *   co-Author :::   Juan Albarracin
+ *   co-Author :::  Mario Bolaños
+ *   co-Author ::: Sergio Orozco
+ *   co-Author :::  Brian Sterling
  *     Program ::: Bases de Datos
  *  Credential ::: SIST0008-G01:SIV
  */
